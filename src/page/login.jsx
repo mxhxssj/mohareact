786655d9fd1221fb09dcb49412c366b70e6b1729
Mouhamed Boulouf <mohaboulouf@gmail.com>
2026-03-08 05:47:00 +0100

    cama

--- src/page/login.jsx
+++ src/page/login.jsx
@@ -10,7 +10,7 @@ export default function Login({ setPage, setIsLogged }) {
         e.preventDefault();
 
         try {
-            const response = await fetch("http://localhost:3001/login", {
+            const response = await fetch("/login", {
                 method: "POST",
                 headers: {
                     "Content-Type": "application/json",
